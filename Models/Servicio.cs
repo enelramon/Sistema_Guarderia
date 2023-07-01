392a3d9e69b5f8d9bdad86adeb6d2405c72d22cc
Wandy-Parzy <wandypaulino05@gmail.com>
2023-07-01 10:15:42 -0400

    Modelo servicio

---
+++ Models/Servicio.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable // Para quitar el aviso de nulls
+namespace Sistema_Guarderia.Models
+{
+
+    public class Servicio //Entidad
+    {  
+        
+         [Key]
+        public int ServicioId { get; set; }
+        [Required(ErrorMessage = "Ingrese el nombre")]
+        public string NombreServicio { get; set; }
+
+        public DateTime FechaServicio { get; set; }
+
+        [Required(ErrorMessage = "Ingrese el monto a pagar.")]
+        public double PrecioServicio { get; set; }
+
+    }
+}
